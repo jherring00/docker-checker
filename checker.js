e835e615a7cf0ebcdc2484cca1eefe41f0cede24
jherring <jherring@onyxgs.com>
2019-06-07 13:03:47 -0400

    httpifyed

--- checker.js
+++ checker.js
@@ -1,7 +1,18 @@
 var Docker = require('dockerode');
 var docker = new Docker({socketPath: '/var/run/docker.sock'});
+
+var containerList = {names: []}
 docker.listContainers(function (err, containers) {
     containers.forEach(function (containerInfo) {
-        console.log(containerInfo.Names[0].substring(1))
+        containerList.names.push(containerInfo.Names[0].substring(1))
     });
 });
+var http = require('http');
+http.createServer(((req, res) => {
+    res.writeHead(200, {'Content-Type': 'application/json'});
+    res.end( JSON.stringify(containerList), null, 3 )
+})).listen(8080)
+
+
+
+
