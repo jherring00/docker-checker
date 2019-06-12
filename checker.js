9f57426881d4b08b5fd619add3a170834c83cf9c
jherring <jherring@onyxgs.com>
2019-06-12 11:34:03 -0400

    added additional options

--- checker.js
+++ checker.js
@@ -1,16 +1,38 @@
 var Docker = require('dockerode');
 var docker = new Docker({socketPath: '/var/run/docker.sock'});
+var url = require("url")
+var containerList = { containers: [] }
+var returnList = { containers: [] }
 
-var containerList = {names: []}
 docker.listContainers(function (err, containers) {
     containers.forEach(function (containerInfo) {
-        containerList.names.push(containerInfo.Names[0].substring(1))
+        containerList.containers.push(containerInfo)
     });
 });
 var http = require('http');
 http.createServer(((req, res) => {
     res.writeHead(200, {'Content-Type': 'application/json'});
-    res.end( JSON.stringify(containerList), null, 3 )
+    var q = url.parse(req.url, true).query
+
+    if(q.inspect) {
+        containerList.containers.forEach((container)=>
+        {
+            if(container.Names[0].substring(1)===q.inspect)
+            {
+                returnList.containers.length=0
+                returnList.containers.push(container)
+            }
+        })
+    }
+    else if(q.listNames==='true')
+    {
+        containerList.containers.forEach((container)=>
+        {
+            returnList.containers.length=0
+            returnList.containers.push(container.Names[0].substring(1))
+        })
+    }
+    res.end( JSON.stringify(returnList), null, 3 )
 })).listen(3000,'localhost')
 
 
